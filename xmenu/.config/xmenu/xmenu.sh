92d12567133ea5b929561ebf0a11336d869b903a
Lady <jhdbui@gmail.com>
2022-08-29 23:38:57 -0700

    XMENU: Changed default browser to firefox

--- xmenu/.config/xmenu/xmenu.sh
+++ xmenu/.config/xmenu/xmenu.sh
@@ -2,7 +2,7 @@
 xmenu <<EOF | sh &
 IMG:/home/lady/.icons/white_file_management.png	File Manager   	thunar
 IMG:/usr/share/pixmaps/nvim.png	Text Editor    	fnvim
-IMG:/usr/share/icons/hicolor/32x32/apps/brave-desktop.png	Web Browser    	brave
+IMG:/usr/share/icons/hicolor/32x32/apps/firefox.png	Web Browser    	firefox
 IMG:/home/lady/.icons/white_terminal_icon.png	Terminal 		st
 IMG:/usr/share/pixmaps/spotify-client.png	Spotify 		spotify
 Background 		nitrogen
