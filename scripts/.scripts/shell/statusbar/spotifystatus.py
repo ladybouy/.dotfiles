5fb19c48538083e8cf1af49f301160d5949ee9a1
Lady <jhdbui@gmail.com>
2023-04-07 15:24:49 -0700

    --amend

--- scripts/.scripts/shell/statusbar/spotifystatus.py
+++ scripts/.scripts/shell/statusbar/spotifystatus.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 import dbus
 
+ICON_COLOR="^c#d79921^"
+ICON=""
+TEXT_COLOR="^d^"
 
 try:
     bus = dbus.SessionBus()
     spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
     spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
     props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    print("^c#d79921^^d^ " + str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+    print(ICON_COLOR+ICON+TEXT_COLOR + " "+ str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
 except dbus.exceptions.DBusException:
     exit
