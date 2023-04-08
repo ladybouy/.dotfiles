fea62ced43f0615bb4154adcba54286d5fa81368
lady <jhdbui@gmail.com>
2023-04-08 15:53:16 -0700

    refactored spotify script

--- scripts/.scripts/shell/statusbar/spotifystatus.sh
+++ scripts/.scripts/shell/statusbar/spotifystatus.sh
@@ -10,7 +10,7 @@ SPOTIFY_FORMAT=$(playerctl --player=spotify metadata --format "{{artist}} - {{ti
 SPOTIFY_STATUS=$(playerctl status --format "{{ uc(status) }}")
 SPOTIFY=$(playerctl --player=spotify)
 
-function spotify_toggle() 
+spotify_toggle() 
 {
     if [[ "$SPOTIFY_STATUS" == "PLAYING" ]]; then
         playerctl --player=spotify pause
@@ -21,16 +21,21 @@ function spotify_toggle()
     fi
 }
 
-if [[ "$SPOTIFY_STATUS" == "PLAYING" ]]; then
-    echo "$ICON_COLOR $ICON $PLAY_ICON $TEXT_COLOR"$SPOTIFY_FORMAT" "
-elif [[ "$SPOTIFY_STATUS" == "PAUSED" ]]; then
-    echo "$ICON_COLOR $ICON $PAUSE_ICON $TEXT_COLOR"$SPOTIFY_FORMAT" "
-else 
-    exit
-fi
+spotify_status()
+{
+    if [[ "$SPOTIFY_STATUS" == "PLAYING" ]]; then
+        echo "$ICON_COLOR $ICON $PLAY_ICON $TEXT_COLOR"$SPOTIFY_FORMAT" "
+    elif [[ "$SPOTIFY_STATUS" == "PAUSED" ]]; then
+        echo "$ICON_COLOR $ICON $PAUSE_ICON $TEXT_COLOR"$SPOTIFY_FORMAT" "
+    else 
+        exit
+    fi
+}
 
 case $BLOCK_BUTTON in
     1) playerctl --player=spotify previous;;
     2) spotify_toggle;;
     3) playerctl --player=spotify next;;
 esac
+
+spotify_status
