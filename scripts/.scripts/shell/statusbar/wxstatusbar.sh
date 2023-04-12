2b491c453b09edaa7d4368c8e394f581c0214c1e
lady <jhdbui@gmail.com>
2023-04-11 22:58:25 -0700

    Fixed weather status output, selecttheme works

--- scripts/.scripts/shell/statusbar/wxstatusbar.sh
+++ scripts/.scripts/shell/statusbar/wxstatusbar.sh
@@ -3,4 +3,4 @@
 ICON_COLOR="^c#d79921^"
 TEXT_COLOR="^c#ebdbb2^"
 
-curl -s wttr.in/$LOCATION?format=4 | awk '{ print "^c#d79921^"$6 "^c#ebdbb2^ "$7 }'
+curl -s wttr.in/$LOCATION?format=1 | awk '{ print "^c#d79921^"$1 "^c#ebdbb2^ "$2 }'
