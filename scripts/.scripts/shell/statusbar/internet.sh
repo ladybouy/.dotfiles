c71e32182e0000a7882aca04d8fc140425351cdf
lady <jhdbui@gmail.com>
2023-04-17 01:38:48 -0700

    Fixed ethernet name

--- scripts/.scripts/shell/statusbar/internet.sh
+++ scripts/.scripts/shell/statusbar/internet.sh
@@ -8,13 +8,13 @@ ETHERNET_UP=
 ETHERNET_DOWN=
  
 if [[ $(cat /sys/class/net/w*/operstate) == "down" ]] && \
-    [[ $(cat /sys/class/net/enp35s0/operstate) == "up" ]]; then
+    [[ $(cat /sys/class/net/enp6s0/operstate) == "up" ]]; then
     echo $ICON_COLOR$ETHERNET_UP 
 elif [[ $(cat /sys/class/net/w*/operstate) == "up" ]] && \
     [[ $(cat /sys/class/net/enp35s0/operstate) == "up" ]]; then
     echo $ICON_COLOR$ETHERNET_UP 
 elif [[ $(cat /sys/class/net/w*/operstate) == "up" ]] && \
-    [[ $(cat /sys/class/net/enp35s0/operstate) == "down" ]]; then
+    [[ $(cat /sys/class/net/enp6s0/operstate) == "down" ]]; then
     echo $ICON_COLOR$WIFI_UP $TEXT_COLOR$(awk '/^\s*w/ {print int($3 * 100 /70) "%"}' /proc/net/wireless)
 else 
     echo $ICON_COLOR$ETHERNET_DOWN$WIFI_DOWN
