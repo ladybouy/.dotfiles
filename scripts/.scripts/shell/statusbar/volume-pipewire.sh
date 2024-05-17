27912631ad13305135c3bd678535ea171c532e9d
lady <jhdbui@gmail.com>
2024-05-17 13:47:11 -0700

    DWM: Unified statusbar colors

--- scripts/.scripts/shell/statusbar/volume-pipewire.sh
+++ scripts/.scripts/shell/statusbar/volume-pipewire.sh
@@ -5,7 +5,7 @@ source $HOME/.themes/statusbar_colors.sh
 BAR_BG_COLOR="^b"$DARK_BACKGROUND"^"
 ICON_BG_COLOR="^b"$CYAN_DARK"^"
 ICON_COLOR="^c"$DARK_BACKGROUND"^"
-BG_COLOR="^b"$CYAN_LIGHT"^"
+BG_COLOR="^b"$CYAN_DARK"^"
 TEXT_COLOR="^c"$DARK_BACKGROUND"^"
 ARROW_COLOR="^c"$CYAN_DARK"^"
 POWERLINE_COLOR="^c"$CYAN_DARK"^"
@@ -17,10 +17,10 @@ RIGHT_ROUND='\ue0b4'
 echo -e $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | 
     awk -v icon_bg_color="$ICON_BG_COLOR" -v icon_color="$ICON_COLOR" -v bg_color="$BG_COLOR" -v text_color="$TEXT_COLOR" -v arrow_color=$ARROW_COLOR -v bar_bg_color="$BAR_BG_COLOR" -v powerline_color=$POWERLINE_COLOR -v powerline_color_2=$POWERLINE_COLOR_2 -v left_round=$LEFT_ROUND -v right_round=$RIGHT_ROUND '
         BEGIN {
-            ICONsn =  icon_bg_color icon_color" "  bg_color text_color " "# headphone unplugged, not muted
-            ICONsm =  icon_bg_color icon_color"婢 " bg_color text_color " " # headphone unplugged, muted
-            ICONhn =  icon_bg_color icon_color" "  bg_color text_color " "# headphone plugged in, not muted
-            ICONhm =  icon_bg_color icon_color"婢 " bg_color text_color " " # headphone plugged in, muted
+            ICONsn =  icon_bg_color icon_color""  bg_color text_color " "# headphone unplugged, not muted
+            ICONsm =  icon_bg_color icon_color"婢" bg_color text_color " " # headphone unplugged, muted
+            ICONhn =  icon_bg_color icon_color""  bg_color text_color " "# headphone plugged in, not muted
+            ICONhm =  icon_bg_color icon_color"婢" bg_color text_color " " # headphone plugged in, muted
         }
         {
             if ($NF == "[MUTED]") {
@@ -36,7 +36,7 @@ echo -e $(wpctl get-volume @DEFAULT_AUDIO_SINK@ |
             printf "%s",h ? (m ? ICONhm : ICONhn) : (m ? ICONsm : ICONsn)
             printf volume"%"
             printf bar_bg_color 
-            printf powerline_color_2
+            printf powerline_color
             printf right_round
         }
         '
