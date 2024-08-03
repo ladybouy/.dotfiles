edc2a13b3da446c10b2623f555c75d48e96d9c17
lady <jhdbui@gmail.com>
2024-08-03 12:41:04 -0700

    DWMBLOCKS: Changed wx format

--- scripts/.scripts/shell/statusbar/wxstatusbar.sh
+++ scripts/.scripts/shell/statusbar/wxstatusbar.sh
@@ -3,8 +3,13 @@
 source $HOME/.themes/statusbar_colors.sh
 
 ICON_BG_COLOR="^b"$BLUE_DARK"^"
-BG_COLOR="^b"$DARK_BACKGROUND"^"
+BAR_BG_COLOR="^b"$DARK_BACKGROUND"^"
+BG_COLOR="^b"$BLUE_DARK"^"
+ICON_BG_COLOR="^b"$BLUE_DARK"^"
 ICON_COLOR="^c"$BLACK_DARK"^"
-TEXT_COLOR="^c"$BLUE_DARK"^"
+TEXT_COLOR="^c"$BLACK_DARK"^"
+POWERLINE_COLOR="^c"$BLUE_DARK"^"
+LEFT_ROUND='\ue0b6'
+RIGHT_ROUND='\ue0b4'
 
-echo -e $(curl -s wttr.in/$LOCATION?format=1 | awk -v arrow_color=$ARROW_COLOR -v icon_bg_color=$ICON_BG_COLOR -v bg_color=$BG_COLOR -v icon_color=$ICON_COLOR -v text_color=$TEXT_COLOR '{ print  icon_bg_color icon_color " "$1" " bg_color text_color " "$2 }')
+echo -e $(curl -s wttr.in/$LOCATION?format=1 | awk -v arrow_color=$ARROW_COLOR -v powerline_color=$POWERLINE_COLOR -v bar_bg_color=$BAR_BG_COLOR -v bg_color=$BG_COLOR -v icon_color=$ICON_COLOR -v text_color=$TEXT_COLOR -v left_round=$LEFT_ROUND -v right_round=$RIGHT_ROUND '{ print powerline_color left_round bg_color text_color $1" " $2 bar_bg_color powerline_color right_round}')
